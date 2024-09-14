f715bdf5a77ab38c66b9275d26afad79d37bbc79
MananPoojara <billybutcher215@gmail.com>
2024-09-14 16:48:37 +0530

    update commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
                     <a className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white uppercase">BlockForge</span>
                     </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <ul className="flex flex-wrap items-center mb-6 text-lg font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
                             <a href="#" className="hover:underline me-4 md:me-6">Connect</a>
                         </li>
@@ -21,7 +21,7 @@ export const Footer = () => {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+                <span className="block text-xl text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Blockforge</a>. All Rights Reserved.</span>
             </div>
         </footer>
 
