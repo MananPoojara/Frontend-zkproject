547d2acb143a2a9ae687c229bacb65f386b49e5c
MananPoojara <billybutcher215@gmail.com>
2024-09-14 16:43:13 +0530

    update commit

--- src/sections/Dash.tsx
+++ src/sections/Dash.tsx
@@ -5,10 +5,10 @@ export const Dash = () => {
     return (
         <>
             <HeaderSection />
-            <div className="dark-dashboard">
+            <div className="overflow-clip">
                 <div className="flex justify-center">
                     {data.map((item, index) => (
-                        <div key={index} className="bg-zinc-700 text-white p-4 rounded-md max-w-[700px] mx-9 mt-9 mb-4 shadow-md">
+                        <div key={index} className="bg-zinc-800 text-white p-4 rounded-xl mt-9 mb-4 shadow-md ">
                             <div className="flex justify-between mb-4 ">
                                 <h2 className="text-4xl font-bold">{item.name}</h2>
                                 <div className="text-3xl font-medium">{item.dob}</div>
@@ -18,7 +18,7 @@ export const Dash = () => {
                                 <div className="text-2xl font-medium">{item.adharId}</div>
                             </div>
                             <div className="flex justify-between mb-4">
-                                <div className="text-2xl font-medium">Wallet Address:</div>
+                                <div className="text-2xl font-medium mr-5">Wallet Address:</div>
                                 <div className="text-2xl font-medium">{item.walletAddress}</div>
                             </div>
                         </div>
